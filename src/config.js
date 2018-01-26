9c65e4b0ca7e1690bf4fe974f8b8f80a9eaf74e9
Stav Davis <stav.davis@gmail.com>
2018-01-26 11:58:44 -0800

    updated config file to include production env var for base URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	//env var on Netlify set to: https://politismart-serverside.herokuapp.com (
-	//env vars available on Netlify only after linking to github! - under "Build & Deploy")
+	//env vars available on Netlify only after linking to github! -- under "Build & Deploy")
 	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 
     	"http://localhost:808d0"
 };
